6b681268476d5f867e00aecd07b0436ade2c0d2f
Rico29 <cedric.bassaget@gmail.com>
2024-12-12 08:38:24 +0100

    creation plugin pour ocnos

---
+++ src/network/ocnos/snmp/plugin.pm
@@ -0,0 +1,58 @@
+#
+# Copyright 2023 Centreon (http://www.centreon.com/)
+#
+# Centreon is a full-fledged industry-strength solution that meets
+# the needs in IT infrastructure and application monitoring for
+# service performance.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+package network::ocnos::snmp::plugin;
+
+use strict;
+use warnings;
+use base qw(centreon::plugins::script_snmp);
+
+sub new {
+    my ( $class, %options ) = @_;
+    my $self = $class->SUPER::new(package => __PACKAGE__, %options);
+    bless $self, $class;
+
+    $self->{modes} = {
+        'cpu'              => 'snmp_standard::mode::cpu',
+        'cpu-detailed'     => 'snmp_standard::mode::cpudetailed',
+	'memory'           => 'snmp_standard::mode::memory',
+	'load'             => 'snmp_standard::mode::loadaverage',
+        'diskio'           => 'snmp_standard::mode::diskio',
+        'interfaces'       => 'snmp_standard::mode::interfaces',
+	'list-interfaces'  => 'snmp_standard::mode::listinterfaces',
+	'time'             => 'snmp_standard::mode::ntp',
+        'tcpcon'           => 'snmp_standard::mode::tcpcon',
+        'udpcon'           => 'snmp_standard::mode::udpcon',
+        'uptime'           => 'snmp_standard::mode::uptime',
+	'environment'	   => 'network::ocnos::snmp::mode::environment',
+    };
+
+    return $self;
+}
+
+1;
+
+__END__
+
+=head1 PLUGIN DESCRIPTION
+
+Check OcNOS software through SNMP.
+
+=cut
